e15f8b2b4939b3a7c3697629715bdb642e22511e
yo9299 <yo9299@gmail.com>
2023-08-10 18:07:12 +0200

    astar

--- julychallenge.hs
+++ julychallenge.hs
@@ -6,9 +6,12 @@ import Debug.Trace
 import qualified Data.List as L
  
 
+data GameState = GameState { _board :: A.Array (Int, Int) Piece, _heuristic :: Int }
 type Board = A.Array (Int, Int) Piece
 type Position = (Int, Int)
 type Piece = Int
+type Queue = [(GameState, Trajectory)]
+data Trajectory = Trajectory { _path :: [(Piece, Move)], _cost :: Int}
 data Move = U | D | L | R
         deriving (Show, Eq) 
 data Color = Blue | Red | Yellow | Orange | Green | Purple | Empty
@@ -38,6 +41,7 @@ initialArray :: A.Array (Int, Int) Int
 initialArray = A.array ((1,1),(5,5)) [((1,1),1), ((1,2),2),((1,3),2), ((1,4),3),((1,5), 4),((2,1),1),((2,2),2), ((2,3),3),((2,4),3),((2,5),4), ((3,1),5), ((3,2),6),((3,3),7), ((3,4),7),((3,5),8),((4,1),9),((4,2),9), ((4,3),8),((4,4),8),((4,5),8), ((5,1),0),((5,2),0), ((5,3),0),((5,4),0),((5,5),0)]
 --initialArray = A.array ((1,1),(3,3)) [((1,1),1), ((1,2),0),((1,3),0),((2,1),0),((2,2),0),((2,3),0),((3,1),0),((3,2),0),((3,3),0)]
 
+initialState = GameState initialArray (distance initialArray)
 
 {-initialArray :: Board
 --initialArray = A.array ((1,1),(5,5)) [((1,1),1), ((1,2),2),((1,3),2), ((1,4),3),((1,5), 4),((2,1),1),((2,2),2), ((2,3),3),((2,4),3),((2,5),4), ((3,1),5), ((3,2),6),((3,3),7), ((3,4),7),((3,5),8),((4,1),9),((4,2),9), ((4,3),8),((4,4),8),((4,5),8), ((5,1),0),((5,2),0), ((5,3),0),((5,4),0),((5,5),0)]
@@ -57,9 +61,9 @@ goalArray = A.array ((1,1),(3,3)) [((1,1),0), ((1,2),0),((1,3),0),((2,1),0),((2,
 {-getPiece:: Pieces -> Int -> [Position]
 getPiece pieces k = pieces M.! k-}
 
-getCase :: Position -> State Board Piece 
+getCase :: Position -> State GameState Piece 
 getCase (i,j) = do 
-                cur <- get 
+                cur <- gets _board 
                 let piece = cur A.! (i,j)
                 pure piece 
 
@@ -78,10 +82,9 @@ move piece s k = M.adjust (f) (k) (piece)
     where 
           f = (\listpos -> movePiece listpos s)-}
 
-execLocally :: State Board a -> State Board Board
+execLocally :: State GameState a -> State GameState GameState
 execLocally function = do 
                     cur <- get 
-                    --pure $ runState function
                     result <- function
                     result <- get
                     put cur
@@ -90,9 +93,9 @@ execLocally function = do
                     
 
 
-getPositionsM :: Piece -> State Board [Position]
+getPositionsM :: Piece -> State GameState [Position]
 getPositionsM p = do 
-    cur <- get 
+    cur <- gets _board 
     let alist = A.assocs cur :: [(Position,Piece)]
         flist = filter f alist
         f:: (Position, Piece) -> Bool 
@@ -102,23 +105,23 @@ getPositionsM p = do
     pure posList
 
 
-movePieceM :: Piece -> Move -> State Board ()
+movePieceM :: Piece -> Move -> State GameState ()
 movePieceM p m = do
-            cur <- get
+            cur <- gets _board
             listPos <- getPositionsM p 
             let newPos = movePiece listPos m 
-                newboard2 = cur A.// zip listPos (repeat 0)
-                newboard = newboard2 A.// zip newPos (repeat p)
-            put newboard
+                newboard0 = cur A.// zip listPos (repeat 0)
+                newboard = newboard0 A.// zip newPos (repeat p)
+            modify (\s ->s{_board = newboard, _heuristic = distance newboard})
 
 
-inBoardP :: Board -> (Int,Int) -> Bool 
+inBoardP :: Board  -> (Int,Int) -> Bool 
 inBoardP b (k1,k2) = and [0<k1,  k1<(n1+1), 0<k2, k2<(n2+1)]
     where (_,(n1, n2)) =A.bounds b 
 
-availablePieceMoveM :: Piece -> Move -> State Board Bool
+availablePieceMoveM :: Piece -> Move -> State GameState Bool
 availablePieceMoveM p m = do 
-                cur <- get 
+                cur <- gets _board 
                 listPos <- getPositionsM p 
                 let candidatePos = movePiece listPos m
                     available = and [ (cur A.! k)== 0| k <- candidatePos ] -- checks case is empty
@@ -131,19 +134,18 @@ availablePieceMoveM p m = do
 
 
 
-availablePieceM :: Piece -> State Board [(Piece,Move)]
+availablePieceM :: Piece -> State GameState [(Piece,Move)]
 availablePieceM p = do 
-            cur <- get
-            --available <- [m | m <-[U, D, L, R], availablePieceMoveM p m ]
+            --cur <- gets _board
             available <- filterM (\m -> availablePieceMoveM p m) [U, D, L, R]
             let listav = zip (repeat p ) available
             pure listav
 
 
 
-computeAdjacentM :: State Board [Piece]
+computeAdjacentM :: State GameState [Piece]
 computeAdjacentM = do
-            cur <- get 
+            cur <- gets _board 
             listPos <- getPositionsM 0 --get the list of free cases
             let candidates = concatMap (\pos -> zip (repeat pos) [ U, D, L, R]) listPos
                 adjacents0 = fmap (\(pos, mov) -> moveCase pos mov) candidates
@@ -152,12 +154,11 @@ computeAdjacentM = do
             adjpieces <- mapM (\pos -> getCase pos) adjpieces0 -- retrieve the pieces using `getCase`
             pure $ L.nub adjpieces                                                  
 
-availableM :: State Board [(Piece, Move)]
+availableM :: State GameState [(Piece, Move)]
 availableM = do 
-        cur <- get 
+        --cur <- get 
         candidatePieces <- computeAdjacentM
         candidateMoves <-  mapM  availablePieceM candidatePieces 
-        --let listmoves= candidateMoves
         pure $ concat candidateMoves --i want to concatenate this so i can return [(Piece, Move)]
 
 isGoal :: Board -> Bool
@@ -165,36 +166,34 @@ isGoal b = (colorb == goalArray)
     where colorb = boardToGoal b
 
 
-isGoalM :: State Board Bool
+isGoalM :: State GameState Bool
 isGoalM = do 
-    cur <- get
+    cur <- gets _board
     let goal = isGoal cur
     pure goal 
 
 
 distance :: Board -> Int 
-distance b = length [v | (v,v')<-zip elsb elsgoal, v /= v']
+distance b = (length [v | (v,v')<-zip elsb elsgoal, v /= v']) `div` 8
     --length [k | k<-[0.. (length elsb) -1], (elsb !! k) == (elsgoal !!k)]
     where elsb = A.elems $ boardToGoal b 
           elsgoal = A.elems goalArray
     --where value = sum [ 1 | (b A.! (k1,k2)) == (goalArray A.! (k1,k2)) , 0<k1, k1<2, 0<k2, k2<2]
 
 
-heuristic :: State Board Int 
-heuristic = do 
-        cur <- get 
-        pure $ distance cur 
+heuristic :: State GameState Int 
+heuristic = distance <$> gets _board
 
 
-orderQueue :: [(Board, (Piece, Move))] -> [(Piece, Move)]
+orderQueue :: [(GameState, (Piece, Move))] -> [(Piece, Move)]
 orderQueue myqueue = fmap (snd. fst) (L.sortBy f pairs)
-        where pairs = zip myqueue (fmap distance (fmap fst myqueue))
+        where pairs = zip myqueue (fmap (_heuristic .fst) myqueue)
               f (x,y) (x',y') = y `compare` y'
 
 
-sortMovesM :: [(Piece, Move)] -> State Board [(Piece, Move)]
+sortMovesM :: [(Piece, Move)] -> State GameState [(Piece, Move)]
 sortMovesM listMoves = do 
-                    cur <- get
+                    cur <- gets _board
                     let pieces = fmap fst listMoves
                         moves = fmap snd listMoves 
                     result <- zipWithM (\piece move -> execLocally $ movePieceM piece move) pieces moves
@@ -203,11 +202,40 @@ sortMovesM listMoves = do
                         result' = orderQueue result0
                     pure result'
                     
--- A*? genetic alg
--- get :: State Board Board
--- put :: Board -> State Board () 
+aStar:: Int -> Trajectory -> Queue -> State GameState (Maybe Trajectory)
+aStar maxCost trajectory queue = do
+        goalP <- isGoalM
+        if goalP 
+            then pure $ Just trajectory
+            else aStar' trajectory queue
+    where 
+          aStar' _ [] = pure Nothing
+          aStar' trajectory queue = do
+                newstates <- expand
+                let ((nextstate, nexttrajectory), queue') = select (queue ++ newstates)
+                put nextstate 
+                aStar maxCost (nexttrajectory{ _cost = _cost nexttrajectory -1}) queue'
+          expand :: State GameState Queue
+          expand = do 
+                 listMoves <- availableM -- [(piece, move)]
+                 --movePieceM              -- piece move
+                 expanded <- forM listMoves $ \(p,m) -> do 
+                                            nextstate <- execLocally $ movePieceM p m  
+                                            pure (nextstate, Trajectory ((p,m):_path trajectory) (_cost trajectory  +1))
+                 pure [(gs, traj) | (gs, traj) <- expanded, _cost traj <= maxCost]
+          select :: Queue -> ((GameState, Trajectory), Queue) 
+          select myqueue = extractMin f myqueue
+          f :: (GameState, Trajectory) -> Int 
+          f (gs, traj) = _heuristic gs + _cost traj 
+
+extractMin :: (a-> Int) -> [a] -> (a, [a]) 
+extractMin feval l = foldl f (head l, []) $ tail l 
+    where f (curmin, curlist) el 
+                | feval curmin <= feval el = (curmin, el:curlist)
+                | feval curmin > feval el = (el, curmin:curlist) 
 
 
+{-
 depthFirst::  [Board] -> Int -> State Board (Maybe [(Piece,Move)])
 depthFirst listExplored 0 = pure Nothing
 depthFirst listExplored k = do 
@@ -237,17 +265,19 @@ depthFirst listExplored k = do
                         Just r -> pure $ Just $ (p,m):r
 
 {-breadthFirst:: [Board] -> [(Piece, Move)] -> Int -> State Board (Maybe [(Piece, Move)])
+-- initialize queue to initialArray
 breadthFirst  listExplored queue 0 = pureNothing
 breadthFirst listExplored queue k = do 
                                 cur <- get 
-                                goalPred <- isGoalM 
-                                if goalPred 
+                                listMoves0 <- availableM 
+                                goalPred <- mapM isGoalM listMoves0
+                                if or goalPred 
                                     then pure $ Just []
                                 else do 
-                                    listMoves0 <- execLocally $ availableM $ MovePieceM queue 
                                     let listMoves = queue ++ listMoves0 --get availablemoves of all states in queue
                                         sortedMoves = sortedMoves listMoves -- sort queue
                                         --result <- --call breadthFirst of every node of depth k (breadthFirst should only explore next depth)
+                                    mapM breadthFirst listExplored queue k-1
 -}
 
 depthFirst1::  [Board] -> [(Piece, Move)] -> Int -> State Board (Maybe [(Piece,Move)])
@@ -276,7 +306,8 @@ depthFirst1 listExplored queue k = do
                 then put store >> pure Nothing
                 else do
                     --sortedMoves <- sortMovesM queue
-                    retM <- depthFirst1 (store : listExplored) queue (k-1)
+                    retM <- depthFirst1 (store : listExplored) queue (k-1) --this is wrong
                     case retM of 
                         Nothing -> put store >> pure Nothing 
                         Just r -> pure $ Just $ (p,m):r
+-}
